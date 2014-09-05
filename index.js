217cbb401a47fa0dbb766a2352653bee9c925553
shinnn <snnskwtnb@gmail.com>
2014-09-05 22:02:55 +0900

    simulate the behavior of original fs.readFile

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var ES6Promise = global.Promise || require('es6-promise').Promise;
 module.exports = function fsReadFilePromise() {
   var args = [].slice.call(arguments);
   return new ES6Promise(function(resolve, reject) {
-    fs.readFile.apply(null, args.concat(function(err, buf) {
+    fs.readFile.apply(fs, args.concat(function(err, buf) {
       if (err) {
         reject(err);
         return;
