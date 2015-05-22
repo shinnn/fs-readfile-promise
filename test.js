c348671f72ce7f8faac44fd2e7006153c902bdf6
Shinnosuke Watanabe <snnskwtnb@gmail.com>
2015-05-22 14:08:22 +0900

    use ES.next code for testing

--- test.js
+++ test.js
@@ -1,47 +1,47 @@
 'use strict';
 
-var test = require('tape');
-var readFile = require('./');
+const test = require('tape');
+const readFile = require('.');
 
-test('fsReadFilePromise()', function(t) {
+test('fsReadFilePromise()', t => {
   t.plan(8);
 
-  readFile('.gitattributes').then(function(actual) {
+  readFile('.gitattributes').then(actual => {
     t.deepEqual(actual, new Buffer('* text=auto\n'), 'should read a file.');
   });
 
-  readFile('.gitattributes', 'utf8').then(function(actual) {
+  readFile('.gitattributes', 'utf8').then(actual => {
     t.equal(actual, '* text=auto\n', 'should reflect encoding to the result.');
   });
 
-  readFile('bower.json', null).catch(function(err) {
+  readFile('bower.json', null).catch(err => {
     t.equal(err.code, 'ENOENT', 'should be rejected when the file doesn\'t exist.');
   });
 
-  readFile('node_modules', undefined).catch(function(err) {
+  readFile('node_modules', undefined).catch(err => {
     t.equal(err.code, 'EISDIR', 'should be rejected when it tries to read a directory.');
   });
 
   t.throws(
-    readFile.bind(null, true),
+    () => readFile(true),
     /TypeError.*string/,
     'should throw a type error when the path isn\'t a string.'
   );
 
   t.throws(
-    readFile.bind(null, '.gitattributes', {encoding: 'foo'}),
+    () => readFile('.gitattributes', {encoding: 'foo'}),
     /Unknown encoding: foo/,
     'should throw an error when the encoding is not supported.'
   );
 
   t.throws(
-    readFile.bind(null, '.gitattributes', true),
+    () => readFile('.gitattributes', true),
     /TypeError.*Bad arguments/,
-    'should throw an error when the second argument is not a string or an object.'
+    'should throw a type error when the second argument is not a string or an object.'
   );
 
   t.throws(
-    readFile.bind(null),
+    readFile,
     /TypeError.*string/,
     'should throw a type error when it takes no arguments.'
   );
