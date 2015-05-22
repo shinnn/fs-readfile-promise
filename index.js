87aeb77849c34a4d3c09b9f898e4d189ea06f8c1
Shinnosuke Watanabe <snnskwtnb@gmail.com>
2015-05-22 14:10:07 +0900

    drop Node v0.10 support
    close #1

--- index.js
+++ index.js
@@ -2,8 +2,6 @@
 
 var fs = require('graceful-fs');
 
-var ES6Promise = global.Promise || require('es6-promise').Promise;
-
 module.exports = function fsReadFilePromise(filePath, options) {
   var resolve;
   var reject;
@@ -16,7 +14,7 @@ module.exports = function fsReadFilePromise(filePath, options) {
     resolve(buf);
   });
 
-  return new ES6Promise(function(_resolve, _reject) {
+  return new Promise(function(_resolve, _reject) {
     resolve = _resolve;
     reject = _reject;
   });
