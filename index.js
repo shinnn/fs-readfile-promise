473e9a947c8dbc10626c6a105e419fdd95c7a8c5
Shinnosuke Watanabe <snnskwtnb@gmail.com>
2016-05-25 19:30:57 +0900

    rename a variable
    Result doesn’t always be a buffer.

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ const fs = require('graceful-fs');
 
 module.exports = function fsReadFilePromise(filePath, options) {
   return new Promise((resolve, reject) => {
-    fs.readFile(filePath, options, (err, buf) => {
+    fs.readFile(filePath, options, (err, data) => {
       if (err) {
         reject(err);
         return;
       }
 
-      resolve(buf);
+      resolve(data);
     });
   });
 };
