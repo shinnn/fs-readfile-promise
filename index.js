c233b67a1e0013394d94664acc5c97ab73350237
Shinnosuke Watanabe <snnskwtnb@gmail.com>
2017-12-26 14:49:09 +0900

    switch to async function
    [BREAKING] drop support for Node.js < 7

--- index.js
+++ index.js
@@ -4,18 +4,18 @@ const {readFile} = require('graceful-fs');
 
 const ARG_ERROR = 'Expected 1 or 2 arguments (<string|Buffer|URL|integer>[, <Object|string>])';
 
-module.exports = function fsReadFilePromise(...args) {
+module.exports = async function fsReadFilePromise(...args) {
 	const argLen = args.length;
 
 	if (argLen === 0) {
 		const error = new RangeError(`${ARG_ERROR}, but got no arguments.`);
 		error.code = 'ERR_MISSING_ARGS';
 
-		return Promise.reject(error);
+		throw error;
 	}
 
 	if (argLen > 2) {
-		return Promise.reject(new RangeError(`${ARG_ERROR}, but got ${argLen} arguments.`));
+		throw new RangeError(`${ARG_ERROR}, but got ${argLen} arguments.`);
 	}
 
 	return new Promise((resolve, reject) => {
